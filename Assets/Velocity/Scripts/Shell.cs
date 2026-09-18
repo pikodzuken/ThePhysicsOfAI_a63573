775cf6fe2adfcad4a0016696c76cec8ce4d47a61
Pi <a63573@alunos.ipb.pt>
2026-09-18 01:09:58 +0100

    Acceleration passo 3

--- Assets/Velocity/Scripts/Shell.cs
+++ Assets/Velocity/Scripts/Shell.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
-    public float speed = 3f;
-    float mass = 10f;
-    float force = 1000;
+    float speed = 0f;
+    float ySpeed = 0f;
+    float mass = 10;
+    float force = 2;
+    float drag = 1;
+    float gravity = -9.8f;
+    float gAccel;
     float acceleration;
 
     void OnCollisionEnter(Collision col)
@@ -23,14 +27,16 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        acceleration = force / mass;
+        speed += acceleration * 1;
+        gAccel = gravity / mass;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        acceleration = force / mass;  
-        speed += acceleration * Time.deltaTime;
-        this.transform.Translate(0, 0, speed * Time.deltaTime); 
+        speed += (1 - Time.deltaTime * drag);
+        ySpeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0, ySpeed, Time.deltaTime * speed);
     }
 }
